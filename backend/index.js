df837167fbbf182615d4d092efb9a525b1e6d975
priyashsharma-lab <priyashsharma496@gmail.com>
2026-05-28 12:34:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
 
 
 // Model name
-const MODEL = "meta-llama/llama-3-8b-instruct:free";
+const MODEL = "mistralai/mistral-7b-instruct:free";
 
 // Main API route
 app.post("/upload", upload.single("file"), async (req, res) => {
